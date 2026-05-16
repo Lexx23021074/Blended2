be63f76600200b10f4c94bce265ca09c3eef03d5
Lexx23021074 <lexx23021074@gmail.com>
2026-05-16 20:04:08 +0000

    done

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -238,18 +238,18 @@
 // }
 // fizzBuzz(17);   
 // Варіант Geminy
-function fizzBuzz(num) {
-    for (let i = 1; i <= num; i++) { 
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('fizzbuzz');
-        } else if (i % 3 === 0) {
-            console.log('fizz');
-        } else if (i % 5 === 0) {
-            console.log('buzz');
-        } else {
-            console.log(i); // Виводимо число, якщо воно не підійшло під умови
-        }
-    }
-}
+// function fizzBuzz(num) {
+//     for (let i = 1; i <= num; i++) { 
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzbuzz');
+//         } else if (i % 3 === 0) {
+//             console.log('fizz');
+//         } else if (i % 5 === 0) {
+//             console.log('buzz');
+//         } else {
+//             console.log(i); // Виводимо число, якщо воно не підійшло під умови
+//         }
+//     }
+// }
 
-fizzBuzz(17);
+// fizzBuzz(17);
--- js/lesson-2.js
+++ js/lesson-2.js
@@ -238,26 +238,26 @@
 // Зверніть увагу, що в масиві може бути кілька обʼєктів з однаковою 
 // назвою фрукта, це також треба урахувати.
 
-const fruits = [
-    { name: "Яблуко", price: 45, quantity: 7 },
-    { name: "Апельсин", price: 60, quantity: 4 },
-    { name: "Банан", price: 125, quantity: 8 },
-    { name: "Груша", price: 350, quantity: 2 },
-    { name: "Виноград", price: 440, quantity: 3 },
-    { name: "Банан", price: 125, quantity: 3 },
-];
+// const fruits = [
+//     { name: "Яблуко", price: 45, quantity: 7 },
+//     { name: "Апельсин", price: 60, quantity: 4 },
+//     { name: "Банан", price: 125, quantity: 8 },
+//     { name: "Груша", price: 350, quantity: 2 },
+//     { name: "Виноград", price: 440, quantity: 3 },
+//     { name: "Банан", price: 125, quantity: 3 },
+// ];
 
-function calcTotalPrice(fruits, fruitName) {
-    let totalPrice = 0;
-    for (const fruit of fruits) {
-        if (fruit.name === fruitName) {
-            totalPrice += fruit.price * fruit.quantity;
-        }
+// function calcTotalPrice(fruits, fruitName) {
+//     let totalPrice = 0;
+//     for (const fruit of fruits) {
+//         if (fruit.name === fruitName) {
+//             totalPrice += fruit.price * fruit.quantity;
+//         }
 
-    }
-    return totalPrice;
+//     }
+//     return totalPrice;
     
-}
-console.log(calcTotalPrice(fruits, "Банан"));
-console.log(calcTotalPrice(fruits, "Яблуко"));
-console.log(calcTotalPrice(fruits, "Манго"));
+// }
+// console.log(calcTotalPrice(fruits, "Банан"));
+// console.log(calcTotalPrice(fruits, "Яблуко"));
+// console.log(calcTotalPrice(fruits, "Манго"));
